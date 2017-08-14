207b7a79c9c376398f308a93f705b66134203a76
Edmar Antonio da Costa Junior <junior.rp_ds2@hotmail.com>
2017-08-14 15:50:22 -0300

    implementações na classe LancamentoRepository

--- ConFin.Repository/LancamentoRepository.cs
+++ ConFin.Repository/LancamentoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ConFin.Common.Domain.Dto;
 using ConFin.Common.Repository;
+using ConFin.Common.Repository.Extension;
 using ConFin.Common.Repository.Infra;
 using ConFin.Domain.Lancamento;
 using System;
@@ -15,32 +16,98 @@ namespace ConFin.Repository
 
         private enum Procedures
         {
-            
+            SP_SelLancamentos,
+            SP_SelLancamento,
+            SP_InsLancamento,
+            SP_UpdLancamento,
+            SP_DelLancamento
         }
 
         public IEnumerable<LancamentoDto> GetAll(int idUsuario, int? idConta = null)
         {
-            throw new NotImplementedException();
+            ExecuteProcedure(Procedures.SP_SelLancamentos);
+            AddParameter("IdUsuario", idUsuario);
+            AddParameter("IdConta", idConta);
+            var lancamentos = new List<LancamentoDto>();
+            using (var reader = ExecuteReader())
+                while (reader.Read())
+                    lancamentos.Add(new LancamentoDto
+                    {
+                        Id = reader.ReadAttr<int>("Id"),
+                        Descricao = reader.ReadAttr<string>("Descricao"),
+                        Valor = reader.ReadAttr<decimal>("Valor"),
+                        Data = reader.ReadAttr<DateTime>("Data"),
+                        IdConta = reader.ReadAttr<int>("IdConta"),
+                        IdCategoria = reader.ReadAttr<int>("IdCategoria"),
+                        NomeCategoria = reader.ReadAttr<string>("NomeCategoria"),
+                        CorCategoria = reader.ReadAttr<string>("CorCategoria"),
+                        IndicadorPago = reader.ReadAttr<string>("IndicadorPago")
+                    });
+
+            return lancamentos;
         }
 
         public LancamentoDto Get(int idLancamento)
         {
-            throw new NotImplementedException();
+            ExecuteProcedure(Procedures.SP_SelLancamento);
+            AddParameter("IdLancamento", idLancamento);
+
+            using (var reader = ExecuteReader())
+            {
+                return !reader.Read()
+                    ? null
+                    : new LancamentoDto
+                    {
+                        Id = reader.ReadAttr<int>("Id"),
+                        Descricao = reader.ReadAttr<string>("Descricao"),
+                        Valor = reader.ReadAttr<decimal>("Valor"),
+                        Data = reader.ReadAttr<DateTime>("Data"),
+                        IdConta = reader.ReadAttr<int>("IdConta"),
+                        IdCategoria = reader.ReadAttr<int>("IdCategoria"),
+                        IndicadorPago = reader.ReadAttr<string>("IndicadorPago"),
+
+                        // manut
+                        IdUsuarioCadastro = reader.ReadAttr<int>("IdUsuarioCadastro"),
+                        NomeUsuarioCadastro = reader.ReadAttr<string>("NomeUsuarioCadastro"),
+                        DataCadastro = reader.ReadAttr<DateTime>("DataCadastro"),
+                        IdUsuarioUltimaAlteracao = reader.ReadAttr<int?>("IdUsuarioUltimaAlteracao"),
+                        NomeUsuarioUltimaAlteracao = reader.ReadAttr<string>("NomeUsuarioUltimaAlteracao"),
+                        DataUltimaAlteracao = reader.ReadAttr<DateTime?>("DataUltimaAlteracao")
+                    };
+            }
         }
 
         public void Post(LancamentoDto lancamento)
         {
-            throw new NotImplementedException();
+            ExecuteProcedure(Procedures.SP_InsLancamento);
+            AddParameter("Descricao", lancamento.Descricao);
+            AddParameter("Valor", lancamento.Valor);
+            AddParameter("Data", lancamento.Data);
+            AddParameter("IdConta", lancamento.IdConta);
+            AddParameter("IdCategoria", lancamento.IdCategoria);
+            AddParameter("IndicadorPago", lancamento.IndicadorPago);
+            AddParameter("IdUsuario", lancamento.IdUsuarioCadastro);
+            ExecuteNonQuery();
         }
 
         public void Put(LancamentoDto lancamento)
         {
-            throw new NotImplementedException();
+            ExecuteProcedure(Procedures.SP_UpdLancamento);
+            AddParameter("Descricao", lancamento.Descricao);
+            AddParameter("Valor", lancamento.Valor);
+            AddParameter("Data", lancamento.Data);
+            AddParameter("IdConta", lancamento.IdConta);
+            AddParameter("IdCategoria", lancamento.IdCategoria);
+            AddParameter("IndicadorPago", lancamento.IndicadorPago);
+            AddParameter("IdUsuario", lancamento.IdUsuarioUltimaAlteracao);
+            ExecuteNonQuery();
         }
 
         public void Delete(int idLancamento)
         {
-            throw new NotImplementedException();
+            ExecuteProcedure(Procedures.SP_DelLancamento);
+            AddParameter("IdLancamento", idLancamento);
+            ExecuteNonQuery();
         }
     }
 }
