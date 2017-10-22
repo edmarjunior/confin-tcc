2e9a77880ea3669f274d40184f2834b928b3292f
Edmar Antonio da Costa Junior <junior.rp_ds2@hotmail.com>
2017-10-22 21:09:37 -0200

    removendo metodos desnecessarios da classe Notification

--- ConFin.Common/Domain/Notification.cs
+++ ConFin.Common/Domain/Notification.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace ConFin.Common.Domain
@@ -7,13 +6,12 @@ namespace ConFin.Common.Domain
     public class Notification
     {
         private readonly List<string> _notifications;
-
         public bool Any => _notifications.Any();
         public IEnumerable<string> Get => _notifications;
 
         public Notification()
         {
-            _notifications = _errors = new List<string>();
+            _notifications = new List<string>();
         }
 
         public void Add(params string[] notification)
@@ -25,30 +23,6 @@ namespace ConFin.Common.Domain
         {
             Add(notification);
             return default(T);
-        }
-
-        // * * * * * * * * * * * * * * * O L D * * * * * * * * * * * * * *
-        private readonly List<string> _errors;
-
-        [Obsolete("Use \".Any\"")]
-        public bool HasErrors => _errors.Any();
-
-        [Obsolete("Use \".Get\"")]
-        public IEnumerable<string> GetErrors()
-        {
-            return _errors;
-        }
-
-        [Obsolete("Use \".Add()\"")]
-        public void AddError(string error)
-        {
-            _errors.Add(error);
-        }
-
-        [Obsolete("Use \".Add()\"")]
-        public void AddErrors(IEnumerable<string> errors)
-        {
-            _errors.AddRange(errors);
         }
     }
 }
