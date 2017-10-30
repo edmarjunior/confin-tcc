a41fdcb592bd0411b936a0d89f79439916de3aba
Edmar Antonio da Costa Junior <junior.rp_ds2@hotmail.com>
2017-10-30 01:45:17 -0200

    teste para geração de relatorio de lançamentos

--- ConFin.Web/Controllers/Relatorios/RelLancamentoController.cs
+++ ConFin.Web/Controllers/Relatorios/RelLancamentoController.cs
@@ -1,5 +1,7 @@
 ﻿using ConFin.Common.Web;
+using OfficeOpenXml;
 using System;
+using System.IO;
 using System.Web.Mvc;
 
 namespace ConFin.Web.Controllers.Relatorios
@@ -14,7 +16,19 @@ namespace ConFin.Web.Controllers.Relatorios
 
         public ActionResult Gerar(DateTime dataInicial, DateTime dataFinal)
         {
-            return File("C:/teste.xls", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            using (var excelPackage = new ExcelPackage())
+            {
+                var wb = excelPackage.Workbook;
+                var ws = wb.Worksheets.Add("Lancamentos");
+
+                ws.Cells[1, 1].Value = "Data";
+                ws.Cells[1, 2].Value = "Descrição";
+                ws.Cells[1, 3].Value = "Categoria";
+                ws.Cells[1, 4].Value = "Valor";
+
+                return File(new MemoryStream(excelPackage.GetAsByteArray()), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    $"Lancamentos {DateTime.Now:yyMMddHHmmss}.xlsx");
+            }
         }
     }
 }
