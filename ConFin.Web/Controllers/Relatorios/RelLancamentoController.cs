bde6231209dcbd88f016c75a423521f08719ca35
Edmar Antonio da Costa Junior <junior.rp_ds2@hotmail.com>
2017-10-30 01:28:10 -0200

    Substituição do icone de Atualizações e implementações do relatório de lançamentos

---
+++ ConFin.Web/Controllers/Relatorios/RelLancamentoController.cs
@@ -0,0 +1,20 @@
+﻿using ConFin.Common.Web;
+using System;
+using System.Web.Mvc;
+
+namespace ConFin.Web.Controllers.Relatorios
+{
+    [OutputCache(Duration = 0)]
+    public class RelLancamentoController: BaseController
+    {
+        public ActionResult RelLancamento()
+        {
+            return View();
+        }
+
+        public ActionResult Gerar(DateTime dataInicial, DateTime dataFinal)
+        {
+            return File("C:/teste.xls", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        }
+    }
+}
