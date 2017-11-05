93b5df15cecf706541d94048ef80f18afbfcc486
Edmar Antonio da Costa Junior <junior.rp_ds2@hotmail.com>
2017-11-05 03:23:09 -0200

    Alterando a classe Compromisso para herdar de DataManut e terirar atributos já herdados agora

--- ConFin.Common/Domain/Dto/CompromissoDto.cs
+++ ConFin.Common/Domain/Dto/CompromissoDto.cs
@@ -1,17 +1,15 @@
-﻿using System;
+﻿using ConFin.Common.Domain.Auxiliar;
+using System;
 
 namespace ConFin.Common.Domain.Dto
 {
-    public class CompromissoDto
+    public class CompromissoDto : DataManut
     {
         public int Id { get; set; }
         public string Descricao { get; set; }
         public byte IdPeriodo { get; set; }
         public DateTime DataInicio { get; set; }
         public short? TotalParcelasOriginal { get; set; }
-        public int IdUsuarioCadastro { get; set; }
-        public DateTime DataCadastro { get; set; }
         public int IdConta { get; set; }
-
     }
 }
