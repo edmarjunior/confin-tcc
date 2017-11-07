fe09ccf04d0388fcfd9e68ca467ad446af212b5d
Edmar Antonio da Costa Junior <junior.rp_ds2@hotmail.com>
2017-11-07 02:04:32 -0200

    implementação para envio de e-mail no Exception Filter da API

--- ConFin.Common/Api/Infra/ExceptionFilter.cs
+++ ConFin.Common/Api/Infra/ExceptionFilter.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using ConFin.Common.Domain.Auxiliar;
+using System;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http.Filters;
 
@@ -9,12 +11,29 @@ namespace ConFin.Common.Api.Infra
 
         public override void OnException(HttpActionExecutedContext actionExecutedContext)
         {
-            actionExecutedContext.Response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+           
+            try
             {
-                Content = new StringContent("Ocorreu um erro ao realizar a operação. Já estamos trabalhando para corrigir.")
-            };
+                var body = $"Ocorreu uma falha na api-confin: {actionExecutedContext.ActionContext.Request.RequestUri}{Environment.NewLine}" +
+                      $"Data......: {DateTime.Now}.{Environment.NewLine}" +
+                      $"Exception.: {actionExecutedContext.Exception.Message}{Environment.NewLine}" +
+                      $"StackTrace: {actionExecutedContext.Exception.StackTrace}";
 
-            base.OnException(actionExecutedContext);
+                Email.Enviar("Exception Filter API", "confinpessoal@outlook.com", body, "ConFin automático");
+
+                actionExecutedContext.Response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent("Ocorreu um erro ao realizar a operação. Já estamos trabalhando para corrigir.")
+                };
+            }
+            catch (Exception ex)
+            {
+                actionExecutedContext.Response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent("Ocorreu um erro ao realizar a operação. Já estamos trabalhando para corrigir.")
+                };
+            }
+            
         }
     }
 }
