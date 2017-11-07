c09de148084a4d1b0e8ab56ecae479b24fa1dfb8
Edmar Antonio da Costa Junior <junior.rp_ds2@hotmail.com>
2017-11-07 02:02:13 -0200

    criação de classe statica para envio de e-mail

---
+++ ConFin.Common/Domain/Auxiliar/Email.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ConFin.Common.Domain.Auxiliar
+{
+    public static class Email
+    {
+        public static void Enviar(string subject, string emailTo, string body, string nomeTo = null)
+        {
+            var client = new SmtpClient
+            {
+                //UseDefaultCredentials = false,
+                Host = "smtp-mail.outlook.com",
+                EnableSsl = true,
+                Credentials = new NetworkCredential("confinpessoal@outlook.com", "teste321"),
+                Port = 587
+            };
+
+            var mail = new MailMessage
+            {
+                Sender = new MailAddress(emailTo, nomeTo),
+                From = new MailAddress("confinpessoal@outlook.com", "ConFin automático"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            mail.To.Add(new MailAddress(emailTo, nomeTo));
+            client.Send(mail);
+        }
+    }
+}
